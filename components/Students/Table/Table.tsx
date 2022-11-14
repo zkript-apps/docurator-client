d834f7b122e18ae584f0556165641b03670e93c8
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2022-11-15 03:02:06 +0800

    feat: added table pagination

--- components/Students/Table/Table.tsx
+++ components/Students/Table/Table.tsx
@@ -1,17 +1,11 @@
 /* eslint-disable react/jsx-key */
 import React, { useMemo, useState, useEffect } from "react";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/20/solid'
 
 export const Table = (tableData) => {
   const [newTableData, setNewTableData] = useState([])
   const [newTableColumns, setNewTableColumns] = useState([])
-
-  useEffect(() => {
-    if (tableData.tableColumns && tableData.tableData.items) {
-      setNewTableColumns(tableData.tableColumns)
-      setNewTableData(tableData.tableData.items)
-    }
-  }, [newTableColumns, newTableData, tableData.tableColumns, tableData.tableData.items])
 
   const columns = useMemo(() => newTableColumns, [newTableColumns]);
   const data = useMemo(() => newTableData, [newTableData]);
@@ -20,39 +14,102 @@ export const Table = (tableData) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
     prepareRow,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
+    setPageSize
   } = useTable({
     columns,
     data,
-  });
+  }, usePagination);
+
+  const { pageIndex } = state;
+
+  useEffect(() => {
+    if (tableData.tableColumns && tableData.tableData.items) {
+      setNewTableColumns(tableData.tableColumns)
+      setNewTableData(tableData.tableData.items)
+      setPageSize(15)
+    }
+  }, [newTableColumns, newTableData, tableData.tableColumns, tableData.tableData.items])
 
   return (
     <>
-      <div className="mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
-        <table {...getTableProps()} className="min-w-full divide-x divide-gray-300">
+      <div className="mt-8 overflow-auto divide-y divide-gray-300 shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
+        <table {...getTableProps()} className="min-w-full divide-y divide-gray-300 table-auto">
           <thead className="bg-gray-50">
             {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()} >
+              <tr {...headerGroup.getHeaderGroupProps()} className="bg-white divide-y divide-gray-200">
                 {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()} className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">{column.render("Header")}</th>
+                  <th {...column.getHeaderProps()} className="py-3.5 pl-4 pr-1 text-left text-sm font-semibold text-gray-900 sm:pl-6">{column.render("Header")}</th>
                 ))}
               </tr>
             ))}
           </thead>
           <tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
                   {row.cells.map((cell) => {
-                    return <td {...cell.getCellProps()} className="w-full py-4 pl-4 pr-3 text-sm text-gray-900 max-w-0 sm:w-auto sm:max-w-none sm:pl-6">{cell.render("Cell")}</td>;
+                    return <td {...cell.getCellProps()} className="py-4 pl-4 pr-3 text-sm text-gray-900 whitespace-nowrap sm:pl-6 ">{cell.render("Cell")}</td>;
                   })}
                 </tr>
               );
             })}
           </tbody>
         </table>
+        <div className="flex justify-end">
+          <span className="inline-flex mt-5 mb-5 mr-10 rounded-md isolate">
+            <span className="mr-4 place-self-center drop-shadow-none">
+              Page{" "}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>{" "}
+            </span>
+            <button
+              onClick={() => gotoPage(0)}
+              disabled={!canPreviousPage}
+              type="button"
+              className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 shadow-sm rounded-l-md focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronDoubleLeftIcon className="w-5 h-5" aria-hidden="true" />
+            </button>
+            <button
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+              type="button"
+              className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 shadow-sm focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => nextPage()}
+              disabled={!canPreviousPage}
+              type="button"
+              className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 shadow-sm focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              Next
+            </button>
+            <button
+              onClick={() => gotoPage(pageCount - 1)}
+              disabled={!canNextPage}
+              type="button"
+              className="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 shadow-sm rounded-r-md focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronDoubleRightIcon className="w-5 h-5" aria-hidden="true" />
+            </button>
+          </span>
+        </div>
       </div>
     </>
   );
