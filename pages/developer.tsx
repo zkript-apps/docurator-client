c41e30a79ca41c555d7e8313da65bfe0195b978b
Kylle Varon Madrigal <kv.madrigal08@gmail.com>
2022-12-13 13:47:49 +0800

    feat: added developer page and api keys

--- pages/developer.tsx
+++ pages/developer.tsx
@@ -1,7 +1,8 @@
+import Dashboard from "../components/Dashboard"
 
 const Developer = () => {
     return (
-        <h1 className="text-6xl font-bold tracking-tight text-indigo-500">Developer</h1>
+        <Dashboard currentPage={"Developer"} />
     )
 }
 
